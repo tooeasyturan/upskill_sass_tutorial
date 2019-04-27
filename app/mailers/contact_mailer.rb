b9116f90eab5218e6c2bc41c0f4bed29d8266290
Josh Turan <turanj92@gmail.com>
2019-04-27 11:42:32 +0000

    Fixed naming errors

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,10 @@
 class ContactMailer < ActionMailer::Base
   default to: 'jturan92@gmail.com'
   
-  def contact_email(name, email, body)
+def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
-    
     mail(from: email, subject: 'Contact Form Message')
   end
 end
